f57f252f843bad628436955c06178f20587200a7
michaelb <michaelpb@gmail.com>
2023-12-28 11:18:41 -0800

    .

---
+++ static/libraries/shop/PlanMenu-short.js
@@ -0,0 +1,16 @@
+/* JSON data, but evaluated as a JS expression */
+
+/* NOTE: Currently disabled */
+/*[
+    {
+        title: 'Virtual Bootcamp',
+        lectures: 'Prerecorded',
+        crossedPrice: '$186',
+        price: 99,
+        length: 'LIFETIME UPDATES',
+        summary: `Our classic bootcamp experience, now as a self-paced,
+        offline-friendly coding bootcamp!`,
+    },
+]*/
+
+[]
